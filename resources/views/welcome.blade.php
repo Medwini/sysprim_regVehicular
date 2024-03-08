5128fd512c1071b75646a1e347e11c84a77c7d22
Medwini <medwhidalgo@gmail.com>
2024-03-08 18:39:32 -0400

    Agregando estilos y contenedores a la pantalla principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,17 @@
 @section('title', 'Sysprim - Inicio')
 
 @section('content')
-    <nav>
-        <a href="/marcas">Marcas</a>
-        <a href="/modelos">Modelos</a>
-        <a href="/vehiculos">Vehiculos</a>
-    </nav>
+
+    <div class="contenedor">
+        <div class="cont_menup">
+            <div class="bg-imagen">
+    
+            </div>
+            <nav class="cont_inicio">
+                <a href="/marcas" class="btn_inicio">Marcas</a>
+                <a href="/modelos" class="btn_inicio">Modelos</a>
+                <a href="/vehiculos" class="btn_inicio">Vehiculos</a>
+            </nav>
+        </div>
+    </div>
 @endsection
